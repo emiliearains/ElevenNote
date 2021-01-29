ac069cd5d4322b40dc647cf7e9bfed2ee2a49c6a
Emilie Rains <emiliearains@gmail.com>
2021-01-29 16:34:51 -0500

    4.12 commit

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -27,8 +27,6 @@ namespace ElevenNote.WebAPI.Controllers
             var note = noteService.GetNoteById(id);
             return Ok(note);
         }
-
-
         public IHttpActionResult Post(NoteCreate note)
         {
             if (!ModelState.IsValid)
@@ -58,6 +56,15 @@ namespace ElevenNote.WebAPI.Controllers
             if (!service.UpdateNote(note))
                 return InternalServerError();
 
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+
+            if (!service.DeleteNote(id))
+                return InternalServerError();
             return Ok();
         }
 
